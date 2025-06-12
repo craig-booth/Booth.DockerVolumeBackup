f57ae232a278620456df946a36fd2b4b8c5329ba
craig-booth <craig-booth@users.noreply.github.com>
2025-06-12 20:23:43 +0930

    Issue #2: Add image to header (#11)
    Co-authored-by: Craig Booth <craig@boothfamily.id.au>

--- booth.dockervolumebackup.ui/src/layout/Menu.tsx
+++ booth.dockervolumebackup.ui/src/layout/Menu.tsx
@@ -1,4 +1,4 @@
-import { TabNav } from '@radix-ui/themes';
+import { TabNav, Flex, Box, Text } from '@radix-ui/themes';
 import { Link, useMatches } from 'react-router-dom';
 
 
@@ -13,17 +13,26 @@ export default function Menu() {
 	};
 
 	return (
-		<TabNav.Root>
-			<TabNav.Link asChild active={isActive('/backups/')}>
-				<Link to={'backups'}>Backups</Link>
-			</TabNav.Link>
-			<TabNav.Link asChild active={isActive('/volumes/')}>
-				<Link to={'volumes'}>Volumes</Link>
-			</TabNav.Link>
-			<TabNav.Link asChild active={isActive('/schedules/')}>
-				<Link to={'schedules'}>Schedules</Link>
-			</TabNav.Link>
-		</TabNav.Root>
+		<Flex direction="column" >
+			<Flex direction="row">
+				<Box px="3"><img src="/Booth.png" width="20px" height="20px" /></Box>
+				<Box><Text size="3" weight="bold">Volume Backup</Text></Box>
+			</Flex>
+			<Box width="100%">
+				<TabNav.Root>
+					<TabNav.Link asChild active={isActive('/backups/')}>
+						<Link to={'backups'}>Backups</Link>
+					</TabNav.Link>
+					<TabNav.Link asChild active={isActive('/volumes/')}>
+						<Link to={'volumes'}>Volumes</Link>
+					</TabNav.Link>
+					<TabNav.Link asChild active={isActive('/schedules/')}>
+						<Link to={'schedules'}>Schedules</Link>
+					</TabNav.Link>
+				</TabNav.Root>
+			</Box>
+		</Flex>
+
 
 	);
 }
